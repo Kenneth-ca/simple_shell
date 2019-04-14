83f6beaa3cce749e1664ccbb9c483d93bcdaa686
Yesid <yesid.yag@gmail.com>
2019-04-14 16:30:20 -0500

    [Fix] betty code

--- comand_prompt.c
+++ comand_prompt.c
@@ -1,10 +1,18 @@
 #include "shell.h"
 
+/**
+ * shell - Function to generate a prompt
+ * @argv: arguments
+ * @envp: Receive the arguments passed to shell
+ *
+ * Return: No returns in this function
+ */
 void shell(char **argv, char *envp[])
 {
     while (1)
     {
         if (isatty(0))
-            write(STDOUT_FILENO, "$(╯°□°）╯ ", strlen("$(╯°□°）╯ "));
+            write(STDOUT_FILENO, "$(╯°□°）╯ "
+            , strlen("$(╯°□°）╯ "));
     }
 }
