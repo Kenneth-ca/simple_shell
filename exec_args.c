643ab2ad0763afcd06b90b2c4229b385aa506739
yawzyag <yesid.yag@gmail.com>
2019-04-16 02:33:07 +0000

    [Fix] betty and problem whit args, free buffer, no more problems whit valgrind

--- exec_args.c
+++ exec_args.c
@@ -11,39 +11,36 @@
  */
 void exec_args(char *buff, char **argv, char **pars, char **env, paths_t *path)
 {
-	int process, status = 0;
-	pid_t pid;
+	int status;
+	pid_t pid, tpid;
 	char *text_parsed;
 
-	text_parsed = check_path(pars, path);
+	/**
+	 * Forking a child
+	 */
 	pid = fork();
-	if (pid == -1)
+
+	if (pid == 0)
 	{
-		perror("Fork failed");
-		free_list(path);
-		free(buff);
-		_exit(1);
-	}
-	else if (pid == 0)
-	{
-		process = execve(text_parsed, pars, env);
-		if (process < 0)
+		text_parsed = check_path(pars, path);
+		if (execve(text_parsed, pars, env) == -1)
 		{
 			write(STDERR_FILENO, argv[0], _strlen(argv[0]));
 			write(STDERR_FILENO, ": ", 2);
 			write(STDERR_FILENO, "1", 1);
 			write(STDERR_FILENO, ": ", 2);
-			write(STDERR_FILENO, pars[0], _strlen(pars[0]));
+			write(STDERR_FILENO, text_parsed, _strlen(text_parsed));
 			write(STDERR_FILENO, ": not found\n", 13);
-			exit(127);
+			free(buff);
+			free_list(path);
 		}
+		exit(0);
 	}
 	else
 	{
-		/**
-		 * waiting for child to terminate
-		 */
-		wait(&status);
-		free(text_parsed);
+		do {
+			tpid = wait(&status);
+		} while (tpid != pid);
 	}
+	free(buff);
 }
